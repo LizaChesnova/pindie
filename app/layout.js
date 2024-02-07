3d3cd7fc7da7b6d5765217afcafceaa7ef4fd721
Лиза Чеснова <lizacesnova@MacBook-Air-Liza.local>
2024-02-07 15:22:57 +0300

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+
+import "./globals.css";
+
+
+
+export const metadata = {
+  title: "Pindie",
+  description: "Портал инди-игр от студентов Яндекс Практикума",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>
+        <header>{/*Содержание шапки*/}</header>
+        {children}
+        <footer>{/*Содержание подвала */}</footer>
+      </body>
+    </html>
+  );
+}
