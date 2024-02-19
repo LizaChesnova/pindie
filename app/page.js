b831d44c7dbb3d94b328923ee361ce9231638bb0
LizaChesnova <liza061004@icloud.com>
2024-02-19 12:40:17 +0300

    yes

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
 
-
+import { getGamesByCategory } from "./data/data-utils";
 import { Banner } from "./components/Banner/Banner";
 import { Promo } from "./components/Promo/Promo";
-import { PopularCardslist } from "./components/CardsList/PopularCardsList";
-import { NewCardsList } from "./components/CardsList/NewCardsList";
+
+import { CardsList } from "./components/CardsList/CardsList";
+
 
 export default function Home() {
+  const popularGames = getGamesByCategory("popular");
+  const newGames = getGamesByCategory("new");
   return (
+
     <main className="main">
-      <Banner/>
-      <PopularCardslist/>
-      <NewCardsList/>
-      <Promo/>
+      <Banner />
+      <CardsList id="popular" title="Популярные" data={popularGames} />
+      <CardsList id="new" title="Новинки" data={newGames} />
+      <Promo />
     </main>
   );
 }
