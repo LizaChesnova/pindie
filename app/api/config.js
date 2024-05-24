b04bafccd3164a3b842d867b4e4c5594f226636d
LizaChesnova <liza061004@icloud.com>
2024-05-24 18:25:05 +0300

    новый api - адрес

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'pindieadminpanel.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://pindieadminpanel.nomoredomainswork.ru//api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
